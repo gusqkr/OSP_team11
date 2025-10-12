a44080b01a42c3cdfb2ae876d3aec2b6681ae3bf
gusqkr <hyeon05@ewha.ac.kr>
2025-10-12 19:35:30 +0900

    기능: 상품 등록 기능 초기 구현 및 폼-Flask 연결
    - 신규 상품 등록을 위한 reg_items.html 폼 파일 생성.
    - Flask 라우트 (/submit_item_post)를 POST 방식으로 연결하여 폼 데이터 및 파일 수신 로직 추가

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sys
 
 application = Flask(__name__)
@@ -26,6 +26,38 @@ def reg_review():
 @application.route('/qna')
 def view_qna():
     return render_template('qna.html')
+
+@application.route('/submit_item')
+def reg_item_submit():
+    name=request.args.get('name')
+    seller=request.args.get('seller')
+    addr=request.args.get('addr')
+    email=request.args.get('email')
+    category=request.args.get('category')
+    card=request.args.get('card')
+    status=request.args.get('status')
+    phone=request.args.get('phone')
+
+    print(name, seller, addr, email, category, card, status, phone)
+    return render_template('reg_items.html')
+
+@application.route('/submit_item_post', methods=['POST'])
+def reg_item_submit_post():
+    image_file = request.files['file']
+    image_file.save("static/images/{}".format(image_file.filename))
+    data=request.form
+
+    print ("\n=========== 입력받은 값 확인 ===========")
+    print ("판매자 아이디 :", data['seller'])
+    print ("상품 이름 :", data['name'])
+    print ("주소 :", data['addr'])
+    print ("카테고리 :", data['category'])
+    print ("이메일 :", data['email'])
+    print ("신용카드 여부 :", data['card'])
+    print ("상품 상태 :", data['status'])
+    print ("휴대폰 번호 :", data['phone'])
+    print ("===============================================\n")
+    return render_template('result.html', data=data, img_path="static/images/{}".format(image_file.filename))
 
 if __name__ == "__main__":
     application.run(debug=True, host='0.0.0.0')
