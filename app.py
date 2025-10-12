0579e957124e3711fa89e88248423eb157c21ad5
gusqkr <hyeon05@ewha.ac.kr>
2025-10-12 17:06:06 +0900

    기능: 기본 페이지 레이아웃 및 네비게이션 구현

--- app.py
+++ app.py
@@ -4,8 +4,28 @@ import sys
 application = Flask(__name__)
 
 @application.route('/')
-def hello():
-    return "Hello, World!"
+def home():
+    return render_template('index.html')
+
+@application.route('/list')
+def view_list():
+    return render_template('list.html')
+
+@application.route('/review')
+def view_review():
+    return render_template('review.html')
+
+@application.route('/reg_items')
+def reg_item():
+    return render_template('reg_items.html')
+
+@application.route('/reg_reviews')
+def reg_review():
+    return render_template('reg_reviews.html')
+
+@application.route('/qna')
+def view_qna():
+    return render_template('qna.html')
 
 if __name__ == "__main__":
     application.run(debug=True, host='0.0.0.0')
