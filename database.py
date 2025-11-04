652798ba14903816786f3a588e42cbedc40c141c
kimyujin8575 <kimyujin8575@naver.com>
2025-11-04 10:46:55 +0900

    Remove user insertion and duplicate check methods
    Removed user insertion and duplicate check methods.

--- database.py
+++ database.py
@@ -23,32 +23,4 @@ class DBhandler:
         print(data, img_path)
         return True
 
-def insert_user(self, data, pw):
-        user_info = {
-            "id": data['id'],
-            "pw": pw,
-            "nickname": data['nickname']
-        }
 
-    
-        if self.user_duplicate_check(str(data['id'])):
-            self.db.child("user").push(user_info)
-            print("New user added:", data)
-            return True
-        else:
-            print("User ID already exists:", data['id'])
-            return False
-
-    def user_duplicate_check(self, id_string):
-        users = self.db.child("user").get()
-        print("users###", users.val())
-
-
-        if str(users.val()) == "None":
-            return True
-        else:
-            for res in users.each():
-                value = res.val()
-                if value['id'] == id_string:
-                    return False
-            return True
