82416e6ebbf73ebd91db73bf6d2b67e1560052fd
gusqkr <hyeon05@ewha.ac.kr>
2025-11-09 19:24:04 +0900

    10주차 그룹실습과제2 코드 작성

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ class DBhandler:
     def __init__(self):
         with open('./authentication/firebase_auth.json') as f:
             config=json.load(f)
-            
+
         firebase = pyrebase.initialize_app(config)
         self.db = firebase.database()
     
@@ -23,3 +23,30 @@ class DBhandler:
         self.db.child("items").child(name).set(item_info)
         print(data, img_path)
         return True
+    
+    def insert_user(self, data, pw):
+        user_info = {
+            "id": data['id'],
+            "pw": pw,
+            "nickname": data['nickname']
+        }
+        if self.user_duplicate_check(str(data['id'])):
+            self.db.child("user").push(user_info)
+            print("New user added:", data)
+            return True
+        else:
+            print("User ID already exists:", data['id'])
+            return False
+
+    def user_duplicate_check(self, id_string):
+        users = self.db.child("user").get()
+        print("users###", users.val())
+        if str(users.val()) == "None":
+            return True
+        else:
+            for res in users.each():
+                value = res.val()
+                
+                if value['id'] == id_string:
+                    return False
+            return True
