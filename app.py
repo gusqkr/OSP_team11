bb74a368d3dd543c675db13d4373eeaa07eac926
gusqkr <hyeon05@ewha.ac.kr>
2025-11-09 17:36:59 +0900

    수정: 다시 작성하기 위해 10주차 과제 코드 일부 삭제

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, session
-from database import DBhandler
-import hashlib
+from flask import Flask, render_template, request
 import sys
 
 application = Flask(__name__)
-application.config['SECRET_KEY'] = "helloosp"
-
-DB = DBhandler()
 
 @application.route('/')
 def home():
@@ -51,7 +46,7 @@ def reg_item_submit_post():
     image_file = request.files['file']
     image_file.save("static/images/{}".format(image_file.filename))
     data=request.form
-    DB.insert_item(data['name'], data, image_file.filename)
+
 
     print ("\n=========== 입력받은 값 확인 ===========")
     print ("판매자 아이디 :", data['seller'])
@@ -63,27 +58,6 @@ def reg_item_submit_post():
     print ("상품 상태 :", data['status'])
     print ("휴대폰 번호 :", data['phone'])
     print ("===============================================\n")
-    return render_template('result.html', data=data, img_path="static/images/{}".format(image_file.filename))
-
-@application.route("/login")
-def login():
-    return render_template("login.html")
-
-@application.route("/signup")
-def signup():
-    return render_template("signup.html")
-
-@application.route("/signup_post", methods=['POST'])
-def register_user():
-    data = request.form
-    pw = request.form['pw']
-    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
-    if DB.insert_user(data, pw_hash):
-        return render_template("login.html")
-    else:
-        flash("user id already exist!")
-        return render_template("signup.html")
-
 
 if __name__ == "__main__":
     application.run(debug=True, host='0.0.0.0')
