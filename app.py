6ecac5be52817a305c96ab115d201892e488898a
DESKTOP-PC7VC7G\lg <jwg0617@gmail.com>
2025-11-21 10:58:57 +0900

    그룹과제 2: 리뷰조회화면 수정

--- app.py
+++ app.py
@@ -38,11 +38,30 @@ def view_list():
 
 @application.route('/review')
 def view_reviews():
-    page = request.args.get("page", 1, type=int)
-    per_page = 9   
-
-    reviews = DB.get_reviews()   
-
+    page = request.args.get("page", 0, type=int)
+    per_page=6 # item count to display per page
+    per_row=3# item count to display per row
+    row_count=int(per_page/per_row)
+    start_idx=per_page*page
+    end_idx=per_page*(page+1)
+    data = DB.get_reviews() #read the table
+    item_counts = len(data)
+    data = dict(list(data.items())[start_idx:end_idx])
+    tot_count = len(data)
+    for i in range(row_count):#last row
+        if (i == row_count-1) and (tot_count%per_row != 0):
+            locals()['data_{}'.format(i)] = dict(list(data.items())[i*per_row:])
+        else: 
+            locals()['data_{}'.format(i)] = dict(list(data.items())[i*per_row:(i+1)*per_row])
+    return render_template("review.html",
+        datas=data.items(),
+        row1=locals()['data_0'].items(),
+        row2=locals()['data_1'].items(),
+        limit=per_page,
+        page=page,
+        page_count=int((item_counts/per_page)+1),
+        total=item_counts)
+'''
     if reviews:
         review_keys = list(reviews.keys())       
         total_count = len(review_keys)          
@@ -59,6 +78,8 @@ def view_reviews():
 
     return render_template('review.html', reviews=page_reviews, page=page, last_page_num=last_page_num)
         
+'''            
+
 
 @application.route('/reg_items')
 def reg_item():
